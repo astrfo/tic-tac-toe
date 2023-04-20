fc4e6648ed9b3e9323847dcca71811ad6b592fa1
Jun <kumejun2000@gmail.com>
2023-04-20 20:36:42 +0900

    選択できないactionが選択されてしまうバグを修正

--- policy/mcts.py
+++ policy/mcts.py
@@ -12,8 +12,9 @@ class MonteCarloTreeSearch:
     def uct_search(self):
         for _ in range(1000):
             selected_node = self.select()
-            reward = selected_node.rollout()
-            selected_node.backup(reward)
+            if not selected_node.env.is_terminal():
+                reward = selected_node.rollout()
+                selected_node.backup(reward)
         best_child_node = self.best_child()
         return best_child_node.env.last_action
 
@@ -29,15 +30,15 @@ class MonteCarloTreeSearch:
         current_node = self
         while not current_node.env.is_terminal():
             if len(current_node.children) < len(current_node.env.get_valid_actions()):
-                return current_node.expand()
+                child_env = current_node.env.copy()
+                return current_node.expand(child_env)
             else:
                 current_node = current_node.best_child()
         return current_node
 
-    def expand(self):
+    def expand(self, child_env):
         untried_actions = [a for a in self.env.get_valid_actions() if a not in [c.env.last_action for c in self.children]]
         action = random.choice(untried_actions)
-        child_env = self.env.copy()
         child_env.play(action)
         new_child = MonteCarloTreeSearch(child_env, parent=self)
         self.children.append(new_child)
@@ -50,5 +51,12 @@ class MonteCarloTreeSearch:
             self.parent.backup(reward)
 
     def best_child(self):
-        choices_weights = [(c.wins / c.visits) + np.sqrt((2 * np.log(self.visits) / c.visits)) for c in self.children]
-        return self.children[np.argmax(choices_weights)]
+        # 子ノードの中で一番良いノードを選択
+        valid_actions = self.env.get_valid_actions()
+        valid_children = [c for c in self.children if c.env.last_action in valid_actions]
+
+        if not valid_children:
+            raise ValueError('No valid children found.')
+        
+        choices_weights = [(c.wins / c.visits) + np.sqrt((2 * np.log(self.visits) / c.visits)) if c.visits != 0 else float('inf') for c in valid_children]
+        return valid_children[np.argmax(choices_weights)]
