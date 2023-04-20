cc7de3862b9f282020bb66b65a4bb6f4850ff091
Jun <kumejun2000@gmail.com>
2023-04-20 16:31:28 +0900

    policy(MCTS)の追加(実際はランダム)

--- simulator.py
+++ simulator.py
@@ -1,12 +1,14 @@
 import numpy as np
 from tictactoe import TicTacToe
+from policy.mcts import MonteCarloTreeSearch
 
 class Simulator:
     def __init__(self):
         self.env = TicTacToe()
+        self.policy = MonteCarloTreeSearch(self.env)
 
     def run(self):
         while not self.env.is_terminal() and not self.env.is_draw():
-            action = np.random.randint(9)
+            action = self.policy.uct_search()
             self.env.play(action)
             print(self.env.board)
